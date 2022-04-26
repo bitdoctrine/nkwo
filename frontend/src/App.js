577965765b6634c2058c1570354d2025891a588d
Friday Okorie <bitdoctrine@gmail.com>
2022-04-26 20:26:27 +0100

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/"> nkwo </a>
+      </header>
+      <main>Products</main>
+    </div>
+  );
+}
+
+export default App;
