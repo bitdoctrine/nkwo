98137d68226cce75bdf50ded80cb2be191df1da5
Friday Okorie <bitdoctrine@gmail.com>
2022-05-03 08:00:15 +0100

    Added jason web token npm parkage for user authentication, created user routes in the server and added JWT SECRET to the environment variables

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoute.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -19,10 +20,15 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/seeds', seedRouter);
-app.use('/api/products', productRouter)
+app.use('/api/products', productRouter);
+app.use('api/users', userRouter);
 
-
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 
